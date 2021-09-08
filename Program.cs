081f51c602cae607428d86e909d55992d5e56b10
Andre Torres Correa <andre.correa@lambda3.com.br>
2021-09-08 14:12:20 -0300

    atualizando gitignore

--- Program.cs
+++ Program.cs
@@ -4,11 +4,11 @@ using designpattenscsharp.Comportamentais.Observer;
 using designpattenscsharp.Comportamentais.State;
 using designpattenscsharp.Comportamentais.Strategy;
 using designpattenscsharp.Comportamentais.TemplateMethod;
+using designpattenscsharp.Comportamentais.Memento;
 using designpattenscsharp.Estruturais.Decorator;
 using designpattenscsharp.Estruturais.Decorator.Banco;
 using designpattenscsharp.Estruturais.Flyweight;
 using designpattenscsharp.Criacionais.Builder;
 using designpattenscsharp.Criacionais.Factory;
-using designpattenscsharp.Comportamentais.Memento;
 
 Memento.Testar();
